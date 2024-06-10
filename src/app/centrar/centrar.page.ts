26a5189ef55ee8695180790c5337d0d6e27711b8
claireconl <64636534+claireconl@users.noreply.github.com>
2024-06-10 14:51:08 +0200

    pagina centrar creada y con CSS

--- src/app/centrar/centrar.page.ts
+++ src/app/centrar/centrar.page.ts
@@ -3,17 +3,24 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-centrar',
   templateUrl: './centrar.page.html',
   styleUrls: ['./centrar.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CentrarPage implements OnInit {
 
   constructor() { }
 
+  swiperSlideChanged(e: any){
+    console.log('changed: ', e);
+  }
   ngOnInit() {
   }
 
