a1a266af393b0b16c203b93c8ea7462e071ca3e3
claireconl <64636534+claireconl@users.noreply.github.com>
2024-06-13 16:29:00 +0200

    pagina desahogarme empezada

--- src/app/desahogarme/desahogarme.page.ts
+++ src/app/desahogarme/desahogarme.page.ts
@@ -2,19 +2,58 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-desahogarme',
   templateUrl: './desahogarme.page.html',
   styleUrls: ['./desahogarme.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class DesahogarmePage implements OnInit {
-
-  constructor() { }
-
+  public today: number;
+  constructor() {
+    this.today = Date.now();
+   }
   ngOnInit() {
   }
 
+  abrirEscribir(){
+    let seccion = document.getElementById("escribir");
+    seccion!.style.display = 'block';
+  }
+
+  abrirAnimo(){
+    let seccion = document.getElementById("animo");
+    seccion!.style.display = 'block';
+   }
+
+  abrirContactos(){
+   let seccion = document.getElementById("contact");
+   seccion!.style.display = 'block';
+  }
+
+  cerrarResto(nombre: string){
+    if(nombre==='contact'){
+      document.getElementById("escribir")!.style.display='none';
+      document.getElementById("animo")!.style.display='none';
+    }
+    else if(nombre==='escribir'){
+      document.getElementById("contact")!.style.display='none';
+      document.getElementById("animo")!.style.display='none';
+    }
+    else if(nombre==='animo'){
+      document.getElementById("escribir")!.style.display='none';
+      document.getElementById("contact")!.style.display='none';
+    }
+    else{
+      document.getElementById("escribir")!.style.display='none';
+      document.getElementById("contact")!.style.display='none';
+      document.getElementById("animo")!.style.display='none';
+    }
+
+  }
 }
